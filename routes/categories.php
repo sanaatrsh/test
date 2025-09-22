cfbf6916bd1e3396c8cb79ec4798e94d0c7f6013
sanaatrsh <sanaatrash09@gmail.com>
2025-09-22 17:50:22 +0300

    edit routes

--- routes/categories.php
+++ routes/categories.php
@@ -7,4 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::resource('/categories', CategoryController::class)->middleware('auth');
 
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::get('/categories/{author}', [CategoryController::class, 'show'])->name('categories.show');
+// Route::get('/categories/{author}', [CategoryController::class, 'show'])->name('categories.show');
